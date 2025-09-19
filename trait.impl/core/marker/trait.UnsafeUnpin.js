c2ad5b35bd07cdf6b0f6698e869343c9d152929e
cdqn5249 <cdqn5249@users.noreply.github.com>
2025-09-19 11:29:41 +0000

    deploy: 1c88ef17a72551bb38f850f728b9cec95f53d732

--- trait.impl/core/marker/trait.UnsafeUnpin.js
+++ trait.impl/core/marker/trait.UnsafeUnpin.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["cdqn",[["impl UnsafeUnpin for <a class=\"struct\" href=\"cdqn/kernel/crypto/struct.CryptoCore.html\" title=\"struct cdqn::kernel::crypto::CryptoCore\">CryptoCore</a>",1,["cdqn::kernel::crypto::CryptoCore"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"cdqn/kernel/crypto/struct.Keypair.html\" title=\"struct cdqn::kernel::crypto::Keypair\">Keypair</a>",1,["cdqn::kernel::crypto::Keypair"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"cdqn/kernel/struct.KDU.html\" title=\"struct cdqn::kernel::KDU\">KDU</a>",1,["cdqn::kernel::KDU"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"cdqn/kernel/struct.License.html\" title=\"struct cdqn::kernel::License\">License</a>",1,["cdqn::kernel::License"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"cdqn/kernel/struct.Metadata.html\" title=\"struct cdqn::kernel::Metadata\">Metadata</a>",1,["cdqn::kernel::Metadata"]]]]]);
+    var implementors = Object.fromEntries([["cdqn",[["impl UnsafeUnpin for <a class=\"struct\" href=\"cdqn/kernel/crypto/struct.CryptoCore.html\" title=\"struct cdqn::kernel::crypto::CryptoCore\">CryptoCore</a>",1,["cdqn::kernel::crypto::CryptoCore"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"cdqn/kernel/crypto/struct.Keypair.html\" title=\"struct cdqn::kernel::crypto::Keypair\">Keypair</a>",1,["cdqn::kernel::crypto::Keypair"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"cdqn/kernel/hlc/struct.HLC.html\" title=\"struct cdqn::kernel::hlc::HLC\">HLC</a>",1,["cdqn::kernel::hlc::HLC"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"cdqn/kernel/struct.KDU.html\" title=\"struct cdqn::kernel::KDU\">KDU</a>",1,["cdqn::kernel::KDU"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"cdqn/kernel/struct.License.html\" title=\"struct cdqn::kernel::License\">License</a>",1,["cdqn::kernel::License"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"cdqn/kernel/struct.Metadata.html\" title=\"struct cdqn::kernel::Metadata\">Metadata</a>",1,["cdqn::kernel::Metadata"]]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[881]}
+//{"start":57,"fragment_lengths":[1044]}
