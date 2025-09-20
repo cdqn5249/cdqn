5af9c6b70f51d32c53da9ec5cf401e837e349293
cdqn5249 <cdqn5249@gmail.com>
2025-09-20 14:08:54 +0700

    Update mod.rs

--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -1,8 +1,18 @@
 // src/runtime/mod.rs
 
+use crate::kernel::KDU;
+
 pub mod entity;
 pub mod network;
-pub mod orchestrator; // New
+pub mod orchestrator;
 pub mod persistence;
-pub mod processor; // New
+pub mod processor;
 pub mod test_entities;
+
+/// Commands that can be sent to the Persistence thread.
+#[derive(Debug)]
+pub enum PersistenceCommand {
+    // We Box the KDU to keep the enum's size small and efficient.
+    WriteKdu(Box<KDU>),
+    Shutdown,
+}
