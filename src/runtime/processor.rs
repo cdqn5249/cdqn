cfb1c37605a399c57b9f196020ea800e7153a557
cdqn5249 <cdqn5249@gmail.com>
2025-09-20 14:02:21 +0700

    Update processor.rs

--- src/runtime/processor.rs
+++ src/runtime/processor.rs
@@ -6,6 +6,7 @@ use std::any::Any;
 use std::collections::{HashMap, VecDeque};
 use std::sync::{Arc, Mutex};
 
+// --- TYPE ALIASES for clarity ---
 type ErasedState = Box<dyn Any + Send>;
 type OutgoingKdus = Vec<KDU>;
 type BehaviorFn = Box<dyn Fn(&ErasedState, KDU) -> (ErasedState, OutgoingKdus) + Send>;
@@ -16,6 +17,8 @@ struct EntityInstance {
     mailbox: Mailbox,
 }
 
+/// The EntityProcessor is the logical heart of the runtime.
+/// It executes the entity behavior functions in a simple, turn-based loop.
 pub struct EntityProcessor {
     entities: HashMap<FQEI, EntityInstance>,
 }
@@ -47,6 +50,7 @@ impl EntityProcessor {
         self.entities.insert(fqei, instance);
     }
 
+    /// Delivers a KDU to the mailbox of a local entity.
     pub fn route_local(&self, target_fqei: &FQEI, kdu: KDU) {
         if let Some(entity) = self.entities.get(target_fqei) {
             entity.mailbox.lock().unwrap().push_back(kdu);
@@ -63,7 +67,7 @@ impl EntityProcessor {
             if let Some(message) = entity.mailbox.lock().unwrap().pop_front() {
                 // The originator of the incoming message is the target for any responses.
                 let response_target = message.originator_fqei.clone();
-                
+
                 let (new_state, outgoing_kuds) = (entity.behavior_fn)(&entity.state, message);
                 entity.state = new_state;
 
