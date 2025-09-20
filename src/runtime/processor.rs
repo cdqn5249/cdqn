0ec46049f8f1a100e48436f6c9dae3fcf8c02454
cdqn5249 <cdqn5249@gmail.com>
2025-09-20 17:00:27 +0700

    Update processor.rs

--- src/runtime/processor.rs
+++ src/runtime/processor.rs
@@ -3,10 +3,9 @@
 use crate::kernel::{FQEI, KDU};
 use crate::runtime::entity::{Entity, Mailbox};
 use std::any::Any;
-use std::collections::{HashMap, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque}; // Import HashSet
 use std::sync::{Arc, Mutex};
 
-// --- TYPE ALIASES for clarity ---
 type ErasedState = Box<dyn Any + Send>;
 type OutgoingKdus = Vec<KDU>;
 type BehaviorFn = Box<dyn Fn(&ErasedState, KDU) -> (ErasedState, OutgoingKdus) + Send>;
@@ -17,16 +16,8 @@ struct EntityInstance {
     mailbox: Mailbox,
 }
 
-/// The EntityProcessor is the logical heart of the runtime.
-/// It executes the entity behavior functions in a simple, turn-based loop.
 pub struct EntityProcessor {
     entities: HashMap<FQEI, EntityInstance>,
-    // Add this to src/runtime/processor.rs, inside impl EntityProcessor
-
-    /// Returns a set of all FQEIs for entities registered locally.
-    pub fn get_local_fqeis(&self) -> std::collections::HashSet<FQEI> {
-        self.entities.keys().cloned().collect()
-    }
 }
 
 impl EntityProcessor {
@@ -56,14 +47,17 @@ impl EntityProcessor {
         self.entities.insert(fqei, instance);
     }
 
-    /// Delivers a KDU to the mailbox of a local entity.
     pub fn route_local(&self, target_fqei: &FQEI, kdu: KDU) {
         if let Some(entity) = self.entities.get(target_fqei) {
             entity.mailbox.lock().unwrap().push_back(kdu);
         }
     }
 
-    /// Runs one "turn" and returns a list of (Target, KDU) tuples.
+    /// Returns a set of all FQEIs for entities registered locally.
+    pub fn get_local_fqeis(&self) -> HashSet<FQEI> {
+        self.entities.keys().cloned().collect()
+    }
+
     pub fn run_turn(&mut self) -> Vec<(FQEI, KDU)> {
         let mut outgoing_routes: Vec<(FQEI, KDU)> = Vec::new();
         let fqei_list: Vec<FQEI> = self.entities.keys().cloned().collect();
@@ -71,12 +65,9 @@ impl EntityProcessor {
         for fqei in fqei_list {
             let entity = self.entities.get_mut(&fqei).unwrap();
             if let Some(message) = entity.mailbox.lock().unwrap().pop_front() {
-                // The originator of the incoming message is the target for any responses.
                 let response_target = message.originator_fqei.clone();
-
                 let (new_state, outgoing_kuds) = (entity.behavior_fn)(&entity.state, message);
                 entity.state = new_state;
-
                 for kdu in outgoing_kuds {
                     outgoing_routes.push((response_target.clone(), kdu));
                 }
